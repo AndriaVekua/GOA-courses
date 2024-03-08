6e15f3ce6f0f9c9b64688b68e5cc50ab91eb45b7
AndriaVekua <andriavekua3@gmail.com>
2024-03-08 00:07:19 -0800

    second commit

---
+++ lesson08.py
@@ -0,0 +1,4 @@
+age =int(input("what is your age: "))
+furture= int(input("how mwny years he want to travel:"))
+print( age + furture )
+
---
+++ lesson8.py
@@ -0,0 +1,8 @@
+math = int(input("enter your math greade: "))
+english = int(input("enter your english greade: "))
+sport = int(input("enter your sport greade: "))
+  
+
+print((math + english + sport)/3)
+
+
